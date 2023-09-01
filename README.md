2200cc47e3f7f65eec1e21cc1ad9b56213b29cc4
Elijah <kcerhajile@gmail.com>
2023-09-01 16:19:05 -0400

    Added all files I've worked on at work

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # monke
 Small monkey project
+Made mostly at work, hence the lack of commits
---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+function getElementViaClick(e){
+	playSound(this);
+};
+
+function clearSound(e){
+	this.classList.remove('click');
+}
+
+function getElementViaKey(e){
+	key = document.querySelector(`[data-key="${e.code}"]`);
+	playSound(key);
+};
+
+function playSound(thisElement) {
+	if(thisElement.classList.contains('click')) { thisElement.classList.remove('click');}; // Noticed problem with buttons sometimes sticking, this solves that issue
+	thisElement.classList.add('click');
+	sound = document.querySelector(`audio[data-key="${thisElement.dataset.key}"]`);
+	sound.load();
+	sound.play();
+}
+
+const boxes = document.querySelectorAll('.img-border'); // boxes being the div elements being used as buttons
+boxes.forEach( box => {
+	box.addEventListener('click', getElementViaClick);
+	box.addEventListener('transitionend', clearSound);
+});
+
+window.addEventListener('keypress', getElementViaKey);
